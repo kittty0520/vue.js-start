4aa32b1846f0f6b745ab7c0e736f00f3e25d2528
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-12-11 01:50:18 +0900

    싱글 파일 컴포넌트 만들기

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<app-header v-bind:title="str" v-on:renew="renewStr"></app-header>
+	</div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+	name: 'App',
+	data: function () {
+		return {
+			str: 'Header',
+		};
+	},
+	components: {
+		'app-header': AppHeader,
+	},
+	methods: {
+		renewStr: function () {
+			this.str = 'hi';
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<button v-on:click="sendEvent">send</button>
+	</header>
+</template>
+
+<script>
+export default {
+	props: ['title'],
+	methods: {
+		sendEvent: function () {
+			this.$emit('renew');
+		},
+	},
+};
+</script>
+
+<style></style>
